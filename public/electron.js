4a4596de10b3156a871af3cdc73c51aa15bd9cbc
tesh254 <ewachira254@gmail.com>
2019-12-28 11:56:16 +0300

    Initial commit

---
+++ public/electron.js
@@ -0,0 +1,43 @@
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require("path");
+const isDev = require("electron-is-dev");
+
+let mainWindow;
+
+function createWindow() {
+  // Define the applications dimension
+  mainWindow = new BrowserWindow({ width: 900, height: 680 });
+  // Determine what to render based on environment
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+  
+  // Show chrome developer tools when in dev environment
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+  // Create event to close window on close
+  mainWindow.on("closed", () => (mainWindow = null));
+}
+
+// On launch create app window
+app.on("ready", createWindow);
+app.on("window-all-closed", () => {
+    // Based on which operating system you are using
+  if (process.platform !== "linux") {
+      // If os not linux, close the app
+      // you can add darwin(mac os), win64 and so many more
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow !== null) {
+    createWindow();
+  }
+});
